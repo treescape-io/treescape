12cdf53c407e0d47f4a184a15b6694a3578e1fc6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-04-01 14:14:43 +0100

    Admin improvements.

--- species/admin.py
+++ species/admin.py
@@ -1,4 +1,8 @@
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
+from django.utils.translation import get_language
 from .models import (
     Family,
     Genus,
@@ -10,91 +14,114 @@ from .models import (
 )
 
 
-class FamilyCommonNameInline(admin.TabularInline):
-    """Inline admin for family common names."""
+# Define a function to retrieve the common name based on the current language.
+def get_common_name(obj, model_common_name):
+    common_names = model_common_name.objects.filter(
+        language=get_language(), **{obj.__class__.__name__.lower(): obj}
+    )
+    if common_names.exists():
+        return common_names.first().name
 
+    # Not available.
+    return ""
+
+
+class FamilyCommonNameInline(admin.TabularInline):
     model = FamilyCommonName
     extra = 1
 
 
 class GenusCommonNameInline(admin.TabularInline):
-    """Inline admin for genus common names."""
-
     model = GenusCommonName
     extra = 1
 
 
 class SpeciesCommonNameInline(admin.TabularInline):
-    """Inline admin for species common names."""
-
     model = SpeciesCommonName
     extra = 1
 
 
 class VarietyInline(admin.TabularInline):
-    """Inline admin for species varieties."""
-
     model = Variety
     extra = 1
 
 
 class SpeciesAdminBase(admin.ModelAdmin):
-    """Base class for species related admins."""
-
     readonly_fields = ["gbif_id"]
 
     def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
         if obj:
-            # Updating existing object
-            return super().get_fields(request, obj)
-
-        # When creating species, only set latin name field.
+            return fields
         return ["latin_name"]
 
     def get_inlines(self, request, obj=None):
         if obj:
-            # Only show inlines once object is created
             return super().get_inlines(request, obj)  # type: ignore
-
         return []
 
     def add_view(self, request, form_url="", extra_context=None):
-        if not extra_context:
-            extra_context = {}
-
+        extra_context = extra_context or {}
         extra_context.update({"show_save": False, "show_save_and_add_another": False})
-
         return super().add_view(request, form_url, extra_context)  # type: ignore
 
     def save_model(self, request, obj, form, change):
         if not change:
-            # Enrich before saving.
             obj.enrich_species_data()
-
         super().save_model(request, obj, form, change)
 
 
 @admin.register(Family)
 class FamilyAdmin(SpeciesAdminBase):
-    """Admin interface for the Family model."""
-
-    list_display = ("latin_name",)
+    list_display = ("latin_name", "common_name")
     inlines = [FamilyCommonNameInline]
+    search_fields = ["latin_name", "familycommonname__name"]
+
+    def common_name(self, obj):
+        return get_common_name(obj, FamilyCommonName)
+
+    common_name.short_description = _("Common Name")
 
 
 @admin.register(Genus)
 class GenusAdmin(SpeciesAdminBase):
-    """Admin interface for the Genus model."""
-
-    list_display = ("latin_name", "family")
+    list_display = ("latin_name", "family", "common_name")
     list_filter = ("family",)
-    search_fields = ("latin_name", "family__latin_name")
+    search_fields = ["latin_name", "genuscommonname__name", "family__latin_name"]
     inlines = [GenusCommonNameInline]
+
+    def common_name(self, obj):
+        return get_common_name(obj, GenusCommonName)
+
+    common_name.short_description = _("Common Name")
 
 
 @admin.register(Species)
 class SpeciesAdmin(SpeciesAdminBase):
-    """Admin interface for the Species model."""
-
-    list_display = ("latin_name",)
+    list_display = ("latin_name", "genus_family_link", "common_name")
+    list_filter = ("genus__family", "genus")
+    search_fields = [
+        "latin_name",
+        "speciescommonname__name",
+        "genus__latin_name",
+        "genus__family__latin_name",
+    ]
     inlines = [SpeciesCommonNameInline, VarietyInline]
+
+    def genus_family_link(self, obj):
+        genus_url = reverse("admin:species_genus_change", args=[obj.genus.pk])
+        family_url = reverse("admin:species_family_change", args=[obj.genus.family.pk])
+        return format_html(
+            '<a href="{}">{}</a> / <a href="{}">{}</a>',
+            genus_url,
+            obj.genus.latin_name,
+            family_url,
+            obj.genus.family.latin_name,
+        )
+
+    genus_family_link.short_description = _("Genus / Family")
+
+    def common_name(self, obj):
+        return get_common_name(obj, SpeciesCommonName)
+
+    common_name.short_description = _("Common Name")
