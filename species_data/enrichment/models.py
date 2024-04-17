db184222e09373b695976fe07e426a16e871ce2c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-04-17 14:07:55 +0100

    Factor out enrichment models.

--- species_data/enrichment/models.py
+++ species_data/enrichment/models.py
@@ -1,26 +1,56 @@
 import decimal
+import enum
 
-from pydantic import BaseModel, Field
+from typing import Optional
 
+from django.utils.text import slugify
 
-class DecimalRangeField(BaseModel):
-    minimum_value: decimal.Decimal
-    typical_value: decimal.Decimal
-    maximum_value: decimal.Decimal
-    confidence: float = Field(gt=0, lt=1)
+from langchain_core.pydantic_v1 import BaseModel, Field, create_model
 
-
-class MultiStringDataField(BaseModel):
-    selected_values: set[str]
-    confidence: float = Field(gt=0, lt=1)
+from species_data.models import GrowthHabit, HumanUse, EcologicalRole, ClimateZone
 
 
-class SpeciesData(BaseModel):
-    height: DecimalRangeField = Field(description="plant height (m)")
-    width: DecimalRangeField = Field(description="canopy width (m)")
-    growth_habits: MultiStringDataField
-    # climate_zones: MultiStringDataField = Field(
-    #     description="Climate zone(s) for species."
-    # )
-    human_uses: MultiStringDataField
-    ecological_role: MultiStringDataField
+def get_species_data_model():
+    """Generates a Pydantic model based on the Django models for plant species data categories."""
+
+    class ConfidenceModel(BaseModel):
+        confidence: decimal.Decimal = Field(gt=0, lt=1, decimal_places=1, max_digits=2)
+
+    class DecimalRangeField(ConfidenceModel):
+        minimum: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
+        typical: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
+        maximum: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
+
+    def generate_django_enum(model) -> enum.Enum:
+        """Generates a string Enum based on the name field of MyModel instances."""
+        # Retrieve all distinct name values from MyModel
+        objects = model.objects.all()
+
+        obj_dict = {slugify(obj): slugify(obj) for obj in objects}
+        obj_enum = enum.Enum(f"{model.__name__}Enum", obj_dict)
+        return obj_enum
+
+    def generate_django_multiselect_field(model):
+        """Generates a field allowing the selection of multiple options based on a given Django model."""
+        model_enum = generate_django_enum(model)
+        model = create_model(
+            f"{model.__name__}Model",
+            __base__=ConfidenceModel,
+            values=(set[model_enum], ...),
+        )
+
+        return model
+
+    class SpeciesData(BaseModel):
+        # TODO: Generate all these fields by iterating over the models.
+        height: DecimalRangeField = Field(description="mature plant height in meters")
+        width: DecimalRangeField = Field(
+            description="mature plant canopy diameter in meters"
+        )
+
+        growth_habits: generate_django_multiselect_field(GrowthHabit)
+        human_uses: generate_django_multiselect_field(HumanUse)
+        ecological_roles: generate_django_multiselect_field(EcologicalRole)
+        climate_zones: generate_django_multiselect_field(ClimateZone)
+
+    return SpeciesData
--- species_data/management/commands/enrich_species_data.py
+++ species_data/management/commands/enrich_species_data.py
@@ -1,66 +1,16 @@
-import decimal
-import enum
 import pprint
-
-from typing import Optional
 
 from tqdm import tqdm
 
 from django.core.management.base import BaseCommand, CommandError
-from django.utils.text import slugify
 
 from langchain_openai import OpenAI
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field, create_model
 
 from plant_species.models import Species
-from species_data.models import GrowthHabit, HumanUse, EcologicalRole, ClimateZone
 
-
-def get_model():
-    """Generates a Pydantic model based on the Django models for plant species data categories."""
-
-    class ConfidenceModel(BaseModel):
-        confidence: decimal.Decimal = Field(gt=0, lt=1, decimal_places=1, max_digits=2)
-
-    class DecimalRangeField(ConfidenceModel):
-        minimum: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
-        typical: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
-        maximum: Optional[decimal.Decimal] = Field(max_digit=3, decimal_places=1)
-
-    def generate_django_enum(model) -> enum.Enum:
-        """Generates a string Enum based on the name field of MyModel instances."""
-        # Retrieve all distinct name values from MyModel
-        objects = model.objects.all()
-
-        obj_dict = {slugify(obj): slugify(obj) for obj in objects}
-        obj_enum = enum.Enum(f"{model.__name__}Enum", obj_dict)
-        return obj_enum
-
-    def generate_django_multiselect_field(model):
-        """Generates a field allowing the selection of multiple options based on a given Django model."""
-        model_enum = generate_django_enum(model)
-        model = create_model(
-            f"{model.__name__}Model",
-            __base__=ConfidenceModel,
-            values=(set[model_enum], ...),
-        )
-
-        return model
-
-    class SpeciesData(BaseModel):
-        height: DecimalRangeField = Field(description="mature plant height in meters")
-        width: DecimalRangeField = Field(
-            description="mature plant canopy diameter in meters"
-        )
-
-        growth_habits: generate_django_multiselect_field(GrowthHabit)
-        human_uses: generate_django_multiselect_field(HumanUse)
-        ecological_roles: generate_django_multiselect_field(EcologicalRole)
-        climate_zones: generate_django_multiselect_field(ClimateZone)
-
-    return SpeciesData
+from species_data.enrichment.models import get_species_data_model
 
 
 def get_llm_chain():
@@ -80,7 +30,7 @@ def get_llm_chain():
     {format_instructions}
     """
 
-    SpeciesData = get_model()
+    SpeciesData = get_species_data_model()
     output_parser = JsonOutputParser(pydantic_object=SpeciesData)
 
     format_instructions = output_parser.get_format_instructions()
@@ -106,6 +56,20 @@ def enrich_species_data(species: Species):
         }
     )
     pprint.pprint(plant_data)
+
+    # TODO:
+    # 1. Auto-generated slugs for all multi-select models.
+    # 2. Idempotently save generated data to models.
+    # 3. Fixture/data migration for Source Type.
+    # 4. Citations for source.
+    # 5. Set source for created data.
+    # 6. Dynamic prompting without source.
+    # 7. Source type confidence.
+    # 8. Add additional sources, confidence-based trumping:
+    #     - a) USDA
+    #     - b) GBIF description
+    #     - c) Trefle
+    #
 
 
 class Command(BaseCommand):
