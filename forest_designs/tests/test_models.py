3d8c950a644eb24d7ed8ee0fb4de383eb58ce169
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-25 16:33:56 +0100

    Fixups in test_save_plant_log().

--- forest_designs/tests/test_models.py
+++ forest_designs/tests/test_models.py
@@ -2,13 +2,12 @@ from django.db import IntegrityError
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from forest_designs.models import Plant, PlantImage, PlantLog
+from forest_designs.models import Plant, PlantImage, PlantLog, PlantLogType
 from plant_species.tests.test_models import SpeciesTestMixin
 
 
 class PlantTestCase(SpeciesTestMixin, TestCase):
     """Test all methods on Plant."""
-
 
     def test_get_name_method(self):
         """Test the get_name method of Plant model."""
@@ -81,15 +80,17 @@ class PlantTestCase(SpeciesTestMixin, TestCase):
             plant.clean()
         except ValidationError:
             self.fail("Plant.clean() raised ValidationError unexpectedly!")
+
     def test_save_plant_log(self):
         """Test the save method of PlantLog model."""
 
         plant = Plant(variety=self.variety, location="POINT(0 0)")
         plant.save()
 
-        plant_log = PlantLog(plant=plant, log_entry="Test log entry")
+        log_type = PlantLogType.objects.create(name="Test Log Type")
+        plant_log = PlantLog(plant=plant, log_type=log_type, notes="Test log entry")
         plant_log.save()
 
         saved_plant_log = PlantLog.objects.get(id=plant_log.pk)
-        self.assertEqual(saved_plant_log.log_entry, "Test log entry")
+        self.assertEqual(saved_plant_log.notes, "Test log entry")
         self.assertEqual(saved_plant_log.plant, plant)
