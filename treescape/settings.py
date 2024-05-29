b86d5d6ef8ea3eee195c85ad2b7720078ae1045e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-29 09:36:58 +0100

    Distribution and loading of species data.

--- treescape/settings.py
+++ treescape/settings.py
@@ -158,3 +158,5 @@ MEDIA_URL = "media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+FIXTURE_DIRS = [BASE_DIR / "fixtures"]
