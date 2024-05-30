cd3bba45e6013ca17e03d183fad3314f5606809e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-30 04:47:04 +0100

    API is no fun without filters!

--- species_data/views.py
+++ species_data/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 
 from plant_species.models import Species, Genus, Family
 
@@ -38,6 +39,23 @@ class SpeciesDataViewSet(viewsets.ModelViewSet):
     lookup_field = "slug"
     queryset = Species.objects.all()
     serializer_class = SpeciesDataSerializer
+    filterset_fields = [
+        # "properties__genus__family",
+        # "properties__genus",
+        "properties__climate_zones",
+        "properties__growth_habits",
+        "properties__human_uses",
+        "properties__ecological_roles",
+    ]
+    search_fields = [
+        "latin_name",
+        "common_names__name",
+        "genus__common_names__name",
+        "genus__family__common_names__name",
+        "genus__latin_name",
+        "genus__family__latin_name",
+    ]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
 
 
 class ClimateZoneViewSet(viewsets.ModelViewSet):
--- treescape/settings.py
+++ treescape/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "django_extensions",
+    "django_filters",
     "drf_redesign",
     "rest_framework",
     "plant_species",
@@ -189,4 +190,5 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
