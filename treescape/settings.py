8b837cbec580e124ac8c5d38da91f56a651367c1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-30 01:28:43 +0100

    Templates to project module.

--- treescape/settings.py
+++ treescape/settings.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = "treescape.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "treescape" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
