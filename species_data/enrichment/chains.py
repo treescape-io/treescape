c4ed232d30473a549202a3bd5a2942e73288bf5f
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-31 16:12:17 +0100

    Prompt tweaks and fixing parser.

--- species_data/enrichment/chains.py
+++ species_data/enrichment/chains.py
@@ -1,6 +1,7 @@
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import OutputFixingParser
 
 
 from .models import get_species_data_model
@@ -19,26 +20,30 @@ def get_enrichment_chain(llm: BaseLanguageModel):
     ```
 
     Important guidelines:
-    - Requested information is optional, do not return values for information not provided in the source.
+    - Requested properties are optional, leave them out if no relevant information is provided in the source.
     - When minimum, maximum or typical values are not specified, return `null`. Never guess a value!
     - Specify length units in meters.
     - Return at most 1 decimal for numbers.
     - Width concerns the canopy width of a plant, not the trunk.
+    - If and only if you return a property, include your level of confidence in the values for that property from the [0.0, 1.0] range.
+    - For categorical properties, only select your answer from provided values.
 
     {format_instructions}
     """
 
     SpeciesData = get_species_data_model()
-    output_parser = JsonOutputParser(pydantic_object=SpeciesData)
 
-    format_instructions = output_parser.get_format_instructions()
+    json_parser = JsonOutputParser(pydantic_object=SpeciesData)
+    fixing_parser = OutputFixingParser.from_llm(parser=json_parser, llm=llm)
 
     prompt = PromptTemplate(
         template=prompt_template,
         input_variables=["latin_name", "source_content"],
-        partial_variables={"format_instructions": format_instructions},
+        partial_variables={
+            "format_instructions": fixing_parser.get_format_instructions()
+        },
     )
 
-    chain = prompt | llm | output_parser
+    chain = prompt | llm | fixing_parser
 
     return chain
