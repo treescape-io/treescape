41fa928fc925ac35d4c28d9b94322bfcacd0051c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-12-02 22:28:09 +0000

    Fixup uuid migrations.
    Still to do: add migration to set primary key.

--- treescape/models.py
+++ treescape/models.py
@@ -6,7 +6,7 @@ from django.contrib.gis.db import models
 class UUIDIndexedModel(models.Model):
     """To prevent version conflicts during editing, use uuid's for indexing."""
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
         abstract = True
