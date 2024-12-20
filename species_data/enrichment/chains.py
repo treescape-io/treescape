613899636f763f9cb1b08a3cd9985f3e0ad9468f
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-12-20 20:34:43 +0100

    Use correct fallback LLM.

--- species_data/enrichment/chains.py
+++ species_data/enrichment/chains.py
@@ -112,7 +112,9 @@ def get_enrichment_chain(config: EnrichmentConfig, data_model: Type[BaseModel]):
     )
     parser = PydanticOutputParser(pydantic_object=data_model)
 
-    retry_parser = RetryWithErrorOutputParser.from_llm(parser=parser, llm=config.llm)
+    retry_parser = RetryWithErrorOutputParser.from_llm(
+        parser=parser, llm=config.fallback_llm
+    )
 
     prompt = ChatPromptTemplate.from_messages(
         [("system", _system_prompt), ("user", _user_prompt)],
